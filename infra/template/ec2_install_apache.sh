6c28a4daf89e8e0af4b7405bfecd2ca8e02ff567
Si Thu Kyaw Tint <guguskyler@gmail.com>
2024-06-19 10:30:15 +0630

    update

--- infra/template/ec2_install_apache.sh
+++ infra/template/ec2_install_apache.sh
@@ -1,13 +1,31 @@
 #! /bin/bash
 # shellcheck disable=SC2164
+
+# Navigate to the home directory
 cd /home/ubuntu
+
+# Update package lists and install Node.js, npm, and Git
 yes | sudo apt update
-yes | sudo apt install python3 python3-pip
-git clone https://github.com/rahulwagh/python-mysql-db-proj-1.git
+yes | sudo apt install nodejs npm git
+
+# Clone the NestJS project repository
+git clone https://github.com/SiThuKyawTint-GuGu/nestjs-mysql.git
+
+# Wait for the clone to complete
 sleep 20
-# shellcheck disable=SC2164
-cd python-mysql-db-proj-1
-pip3 install -r requirements.txt
-echo 'Waiting for 30 seconds before running the app.py'
-setsid python3 -u app.py &
+
+# Navigate to the project directory
+cd nestjs-mysql
+
+# Install project dependencies
+npm install
+
+# Build the project
+npm run build
+
+# Wait for 30 seconds before starting the server
+echo 'Waiting for 30 seconds before starting the server'
 sleep 30
+
+# Start the NestJS application in the background
+setsid npm run start:prod &
